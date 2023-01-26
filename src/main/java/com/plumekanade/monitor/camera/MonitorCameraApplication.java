305363e26281a00cf3dc8fb548e285ce4392c3fe
plumekanade <kanade@plumekanade.com>
2023-01-27 01:25:41 +0800

    初始化

---
+++ src/main/java/com/plumekanade/monitor/camera/MonitorCameraApplication.java
@@ -0,0 +1,13 @@
+package com.plumekanade.monitor.camera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorCameraApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MonitorCameraApplication.class, args);
+  }
+
+}
