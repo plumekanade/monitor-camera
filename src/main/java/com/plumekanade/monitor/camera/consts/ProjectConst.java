37968dd9945f81a9fa9ed774d0a05bc7f0fb386a
plumekanade <kanade@plumekanade>
2023-01-31 18:15:59 +0800

    .

---
+++ src/main/java/com/plumekanade/monitor/camera/consts/ProjectConst.java
@@ -0,0 +1,14 @@
+package com.plumekanade.monitor.camera.consts;
+
+/**
+ * 项目常量类
+ *
+ * @author kanade
+ * @date 2023-01-31
+ */
+public class ProjectConst {
+  // 测试用rtsp视频流地址
+  public static final String RTSP_URL = "";
+  // 单个视频录制时长 秒为单位
+  public static final Long DURATION = 21600L;
+}
